9dc965c9d8b721094c51d2199fb0a040d61fac36
aidant842 <aidant842@gmail.com>
2020-04-13 13:57:55 +0000

    continued fix of email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,36 +1,37 @@
-function sendMail(name,email,request,form){
+function sendMail(name, email, request, form) {
     emailjs.send(
         "aidant842_gmail_com",
         "portfolio",
         {
-                "from_name": name,
-                "from_email": email,
-                "project_request": request
+            "from_name": name,
+            "from_email": email,
+            "project_request": request
         }
     )
-    
-    .then(
-        function (response){
-            console.log("SUCCESS", response);
-            form.reset();
-        },
-        function (error){
-            console.log("ERROR", error);
-        });
+
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                form.reset();
+            },
+            function (error) {
+                console.log("ERROR", error);
+            });
 }
 
-if(document.getElementById("contactForm")){
+document.getElementById("contactForm").addEventListener("submit",
     sendMail(
         contactForm.inputName.value,
         contactForm.inputEmail.value,
         contactForm.textArea.value,
         document.getElementById("contactForm")
     )
-}else{
+);
+document.getElementById("modalContactForm").addEventListener("submit",
     sendMail(
         modalContactForm.modalInputName.value,
         modalContactForm.modalInputEmail.value,
         modalContactForm.modalTextArea.value,
         document.getElementById("modalContactForm")
     )
-}
+);
