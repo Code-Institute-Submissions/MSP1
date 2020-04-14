e1b5823695bbcf7eed747a8abea6b88a24ea826e
aidant842 <aidant842@gmail.com>
2020-04-14 12:06:09 +0000

    fixed and finished email.js, both main contact form and modal contact form work as should

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,37 +1,42 @@
-function sendMail(name, email, request, form) {
-    emailjs.send(
-        "aidant842_gmail_com",
-        "portfolio",
-        {
-            "from_name": name,
-            "from_email": email,
-            "project_request": request
-        }
-    )
+$(document).ready(function () {
+    function sendMail(name, email, request, form) {
+        emailjs.send(
+            "aidant842_gmail_com",
+            "portfolio",
+            {
+                "from_name": name,
+                "from_email": email,
+                "project_request": request
+            }
+        )
 
-        .then(
-            function (response) {
-                console.log("SUCCESS", response);
-                form.reset();
-            },
-            function (error) {
-                console.log("ERROR", error);
-            });
-}
+            .then(
+                function (response) {
+                    console.log("SUCCESS", response);
+                    form.reset();
+                },
+                function (error) {
+                    console.log("ERROR", error);
+                });
+    }
 
-document.getElementById("contactForm").addEventListener("submit",
-    sendMail(
-        contactForm.inputName.value,
-        contactForm.inputEmail.value,
-        contactForm.textArea.value,
-        document.getElementById("contactForm")
-    )
-);
-document.getElementById("modalContactForm").addEventListener("submit",
-    sendMail(
-        modalContactForm.modalInputName.value,
-        modalContactForm.modalInputEmail.value,
-        modalContactForm.modalTextArea.value,
-        document.getElementById("modalContactForm")
-    )
-);
+    document.getElementById("contactForm").onsubmit = function () {
+        sendMail(
+            contactForm.inputName.value,
+            contactForm.inputEmail.value,
+            contactForm.textArea.value,
+            document.getElementById("contactForm")
+        )
+        return false;
+    };
+
+    document.getElementById("modalContactForm").onsubmit = function () {
+        sendMail(
+            modalContactForm.modalInputName.value,
+            modalContactForm.modalInputEmail.value,
+            modalContactForm.modalTextArea.value,
+            document.getElementById("modalContactForm")
+        )
+        return false;
+    };
+});
